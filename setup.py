2302cb444d995b8eb3da658246f1bb0da286db5b
siddheshgunjal <siddhu19@live.com>
2023-10-19 13:06:12 +0530

    initial fix

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-install_requires = ['responder', 'graphql-core==2.3', 'graphene==2.1.8', 'numpy==1.18.0', 'transformers[tf-cpu]==3.0.2', 'rake_nltk', 'nltk','tensorflow_hub','pandas','matplotlib', 'cryptography', 'sqlalchemy_utils', 'sqlalchemy', 'awswrangler==1.7.0']
+install_requires = ['responder','graphql-core==2.2.1','graphene==2.1.8','numpy==1.18.5','matplotlib==3.3.4','pandas==1.1.5','tensorflow==2.3.1','tensorflow_hub','transformers[tf-cpu]==3.0.2','rake_nltk','nltk','cryptography','sqlalchemy_utils','sqlalchemy','awswrangler==1.7.0']
 
 setuptools.setup(
     name="tableqa", # Replace with your own username
-    version="0.0.11",
+    version="0.0.12",
     author="Abhijith Neil Abraham, Fariz Rahman",
     author_email="abhijithneilabrahampk@gmail.com,farizrahman4u@gmail.com",
     description="Tool for querying natural language on tabular data",

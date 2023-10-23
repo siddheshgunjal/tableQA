51338a7eb0a6133de45b07276087b3ce786d0721
siddheshgunjal <siddhu19@live.com>
2023-10-23 16:25:18 +0530

    bump up version to 0.0.12

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-install_requires = ['Cython==3.0.3','responder','graphql-core==2.3.2','graphene==2.1.8','numpy==1.18.5','matplotlib==3.3.4','pandas==1.1.5','protobuf==3.20.2','click==7.1.2','tokenizers==0.8.1.rc2','tensorflow-cpu==2.3.1','tensorflow_hub','rake_nltk','nltk','cryptography','sqlalchemy_utils','sqlalchemy==1.3.24','awswrangler==1.7.0','transformers[tf-cpu]==3.1.0']
+install_requires = ['Cython==3.0.3','responder','graphql-core==2.3.2','graphene==2.1.8','numpy==1.18.5','matplotlib==3.3.4','pandas==1.1.5','cryptography==41.0.4','protobuf==3.20.2','click==7.1.2','tokenizers==0.8.1.rc2','tensorflow-cpu==2.3.1','tensorflow_hub','rake_nltk','nltk','sqlalchemy_utils','sqlalchemy==1.3.24','awswrangler==1.7.0','transformers[tf-cpu]==3.1.0']
 
 setuptools.setup(
     name="tableqa", # Replace with your own username
-    version="0.0.6",
+    version="0.0.12",
     author="Abhijith Neil Abraham, Fariz Rahman",
     author_email="abhijithneilabrahampk@gmail.com,farizrahman4u@gmail.com",
     description="Tool for querying natural language on tabular data",
